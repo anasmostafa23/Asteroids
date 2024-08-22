d26a1e0e7f1095b6c500e0e50e9dd2959b563be8
Anas Afif <anasmostafa23@yahoo.com>
2024-08-22 19:56:00 +0500

    adding player

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 from constants import *
+from player import *
 pygame.init()
 
 
 def main () :
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+    p = player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2,PLAYER_RADIUS)
     while True : 
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                return
+        
         screen.fill((0,0,0))
+        p.draw(screen)
         pygame.display.flip()
+        
+        time.tick(60)
+        dt = time.tick(60)/1000
+
+        
     
 if __name__ == "__main__":
     main()
